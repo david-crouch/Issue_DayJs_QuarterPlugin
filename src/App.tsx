a13b0d6cc97408f5d76dc1b7254484cac14630d3
David Crouch <dcrouch@sensei.cloud>
2020-04-20 16:57:13 +0930

    Demo

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import dayjs from 'dayjs';
+//var quarterOfYear = require('dayjs/plugin/quarterOfYear')
+//dayjs.extend(quarterOfYear)
+
+function App() {
+var now = dayjs();
+var startOfMonth = now.startOf('month')
+var startOfQuarter = now.startOf('quarter')
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Test Only
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
